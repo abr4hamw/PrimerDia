0f21e9aea5725cf01bbccb2a4dbb849c44bbb618
abr4hamw <abraham4woods@gmail.com>
2025-08-26 13:36:53 -0600

    Comentario en el Main

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //Abraham Asaf Jimenez Valerio
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
